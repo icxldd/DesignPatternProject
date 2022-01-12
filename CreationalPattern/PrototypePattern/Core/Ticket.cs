54457161a0544d56123a861be235158e51190885
icxldd <1278245922@qq.com>
2022-01-12 23:15:51 +0800

    原型模式

---
+++ CreationalPattern/PrototypePattern/Core/Ticket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace PrototypePattern.Core
+{
+    [Serializable]
+    public abstract class BaseClone<T> where T : class
+    {
+        public virtual T Clone()
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(memoryStream, this);
+            memoryStream.Position = 0;
+            return (T) formatter.Deserialize(memoryStream);
+        }
+    }
+
+    [Serializable]
+    public class Ticket : BaseClone<Ticket>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ CreationalPattern/PrototypePattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using PrototypePattern.Core;
+
+namespace PrototypePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ticket ticket = new Ticket() {Name = "程先龙", Age = 21};
+
+            Ticket cloneTicket = ticket.Clone();
+
+            Ticket eqTicket = ticket;
+            if (ticket.GetHashCode() == cloneTicket.GetHashCode())
+            {
+                Console.WriteLine("1_是相同的对象");
+            }
+
+
+            if (ticket.GetHashCode() == eqTicket.GetHashCode())
+            {
+                Console.WriteLine("2_是相同的对象");
+            }
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
