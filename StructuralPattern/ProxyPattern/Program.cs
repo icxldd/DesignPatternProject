bcf4fe3c33c65386ced9542a6922edb179d6e09b
icxldd <1278245922@qq.com>
2022-01-13 20:31:29 +0800

    代理模式

---
+++ StructuralPattern/ProxyPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ProxyPattern.Request;
+
+namespace ProxyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpRequestProxy = new HttpRequestProxy();
+            httpRequestProxy.Post();
+        }
+    }
+}
---
+++ StructuralPattern/ProxyPattern/Request/HttpRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ProxyPattern.Request
+{
+    public class HttpRequest
+    {
+        public void Post()
+        {
+            Console.WriteLine("发送Post请求");
+        }
+    }
+
+    public class HttpRequestProxy
+    {
+        private readonly HttpRequest _httpRequest;
+
+        public HttpRequestProxy()
+        {
+            _httpRequest = new HttpRequest();
+        }
+
+        public void Post()
+        {
+            Console.WriteLine("HttpRequestProxy代理拦截，准备执行");
+            _httpRequest.Post();
+            Console.WriteLine("HttpRequestProxy代理拦截，执行完成");
+        }
+
+    }
+}
