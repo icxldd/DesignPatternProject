2e0f8dab40fa2f843f30c2046bd09c397b16ea85
icxldd <1278245922@qq.com>
2022-01-13 19:42:00 +0800

    门面模式

---
+++ StructuralPattern/FacePattern/Core/PlayFace.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FacePattern.Core
+{
+    public class PlayFace
+    {
+        private readonly GoodsManager _goodsManager;
+        private readonly WalletManager _walletManager;
+
+        public PlayFace()
+        {
+            _goodsManager =new GoodsManager();
+            _walletManager = new WalletManager();
+        }
+
+        public void Buy()
+        {
+            _goodsManager.Buy();
+            _walletManager.Buy();
+        }
+    }
+
+    public class GoodsManager
+    {
+        public void Buy()
+        {
+            Console.WriteLine("商品下单");
+        }
+    }
+
+    public class WalletManager
+    {
+        public void Buy()
+        {
+            Console.WriteLine("钱包下单");
+        }
+    }
+
+}
---
+++ StructuralPattern/FacePattern/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FacePattern.Core;
+
+namespace FacePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var playFace = new PlayFace();
+            playFace.Buy();
+        }
+    }
+}
